9b605d25953c8a1529759d327fbd76032105f8a5
Sarbjit Singh <srbcheema1@gmail.com>
2017-03-20 19:25:30 +0530

    initial commit

---
+++ src/puzzle4x4/puzzle4x4.java
@@ -0,0 +1,10 @@
+package puzzle4x4;
+
+public class puzzle4x4 {
+    
+    public static void main(String[] args) {
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true); 
+    }
+    
+}
